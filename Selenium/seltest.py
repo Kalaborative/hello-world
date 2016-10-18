29a6e3caaec0fad2538e9460b9809cc33216bc2f
Drew <kalaborative94@gmail.com>
2016-10-18 14:17:33 -0500

    Updated random query

--- Selenium/seltest.py
+++ Selenium/seltest.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from time import sleep
+from bagmixer import mixBag
 
 # Create a new Chrome session
+mySearch = mixBag()
 driver = webdriver.Chrome()
 driver.implicitly_wait(30)
 driver.maximize_window()
-
 # navigate to the application home page
 driver.get("http://www.google.com")
 
@@ -13,29 +14,28 @@ driver.get("http://www.google.com")
 search_field = driver.find_element_by_name("q")
 
 # enter search keyword and submit
-search_field.send_keys("Selenium WebDriver Interview questions")
+search_field.send_keys(mySearch)
 search_field.submit()
 
 # get the list of elements which are displayed after the search
 # currently on result page using find_elements_by_class_name method
-txts = driver.find_elements_by_xpath('//*[@class="g"]//h3/a')
-
 print "The automation was a success!"
-sleep(2)
-print "It found " + str(len(txts)) + " searches."
-sleep(2)
-print ("\n" * 100)
-print "Displaying results:"
 
-# iterate through each element and print the text that is
-# name of the search
-i = 0
-while (i < 10):
+
+def scrape():
+	txts = driver.find_elements_by_xpath('//*[@class="g"]//h3/a')
+	sleep(2)
+	print "It found " + str(len(txts)) + " searches."
+	sleep(2)
+	print ("\n" * 100)
+	print "Displaying results:"
+
 	for t in txts:
 		print "Title: ", t.text
 		print "Link: ", t.get_attribute('href')
 		print
-		i += 1
 		sleep(3)
 
+
+scrape()
 driver.quit()
